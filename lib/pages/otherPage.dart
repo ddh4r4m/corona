271ca0ff85395268c65ed5f66f46ff38abe8fbf9
Mayank Chaturvedi <chaturvedim4@gmail.com>
2020-04-02 16:37:31 +0530

    dynamic webscrapper added

--- lib/pages/otherPage.dart
+++ lib/pages/otherPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 
 class CasesReport{
   String stateName;
@@ -8,22 +12,78 @@ class CasesReport{
   CasesReport({this.stateName, this.confirmedCases, this.cured, this.death});
 }
 
-String updateStamp="01 April 2020, 09:00 GMT+5:30";
+String updateStamp="No Network";
 
 var data= <CasesReport>[
-  CasesReport(stateName: 'Andhra Pradesh', confirmedCases: 83, cured: 1, death: 0),
-  CasesReport(stateName: 'Andaman and Nicobar Islands', confirmedCases: 10, cured: 0, death: 0),
+  CasesReport(stateName: 'No internet found', confirmedCases: 0, cured: 0, death: 0),
 ];
 
 class OtherPage extends StatefulWidget {
 
   _OtherPageState createState() => _OtherPageState();
 }
-
+Stream classStream(dom.Document document, String tag) async*{
+  for(dom.Element element in document.getElementsByClassName(tag)){
+    yield element;
+  }
+}
+Stream tagStream(dom.Document document, String tag) async*{
+  for(dom.Element element in document.getElementsByTagName(tag)){
+  yield element;
+  }
+}
+List<String> states=[
+  "Andhra Pradesh","Andaman and Nicobar Islands","Assam","Bihar","Chandigarh",
+  "Chhattisgarh","Delhi","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir",
+  "Jharkhand","Karnataka","Kerala","Ladakh","Madhya Pradesh","Maharashtra","Manipur",
+  "Mizoram","Odisha","Puducherry","Punjab","Rajasthan","Tamil Nadu","Telengana",
+  "Uttarakhand","Uttar Pradesh","West Bengal"
+];
 class _OtherPageState extends State<OtherPage>{
+  String newStr=updateStamp;
+  void updateComplete() async{
+    http.Response response = await http.get('https://www.mohfw.gov.in');
+    dom.Document document = parser.parse(response.body);
+    List<String> myList=[];
+    await for (dom.Element element in tagStream(document,'td')){
+      myList.add(element.text);
+    }
+    int myListLen=myList.length;
+    data.clear();
+    for(var stateName in states){
+      for(int index=0;index<myListLen;++index){
+        if(myList[index]==stateName){
+          data.add(
+            CasesReport(
+              stateName: myList[index],
+              confirmedCases: int.parse(myList[index+1]),
+              cured:int.parse(myList[index+2]),
+              death: int.parse(myList[index+3]),
+            )
+          );
+        }
+      }
+    }
+  }
+  void getData() async{
+    http.Response response = await http.get('https://www.mohfw.gov.in');
+    dom.Document document = parser.parse(response.body);
+    await for (dom.Element element in classStream(document,'status-update')){
+      newStr = element.text;
+      return ;
+    }
+    return ;
+  }
 
-  void _refresh() {
-    setState(() {
+  void _refresh() async{
+    newStr=updateStamp;
+    await getData();
+    if(newStr==updateStamp){
+      return ;
+    }
+    setState(() async{
+      await updateComplete();
+      updateStamp=newStr;
     });
   }
   Widget bodyData() => DataTable(
@@ -74,6 +134,7 @@ class _OtherPageState extends State<OtherPage>{
   );
   @override
   Widget build(BuildContext context) {
+    _refresh();
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -98,7 +159,9 @@ class _OtherPageState extends State<OtherPage>{
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        child:Text("Data in GOI website was updated on :\n "+updateStamp),
+        color:BlendMode.colorBurn,
+        child:Text(updateStamp),
+
       ),
     );
   }
